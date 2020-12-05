a846b32c44a7a3c5e49f04e00d4ceeb34c1e513a
Tiago Roxo <tfroxo@hotmail.com>
2020-12-05 22:52:25 +0000

    Add source refs

--- src/test.py
+++ src/test.py
@@ -96,6 +96,54 @@ def combine_masses(dict_m1, dict_m2, list_powerset):
     return normalize_masses_combined(dict_combined_m)
 
 
+# ---------------------------
+
+def gradient_descent(theta_dg, learning_rate, X_train_complex, X_valid_complex):
+
+    valid_error_list = []
+    num_it = 0
+    validation_error_prev = sys.maxsize
+    max_interations_allowed = 2000
+
+    mean_valid_error = 0
+    sd_valid_error = 0
+    v = []
+    t = []
+
+    while True:
+        prediction = np.dot(X_train_complex,theta_dg)
+        theta_dg = theta_dg -(1/len(Y_train)) * learning_rate * (X_train_complex.T.dot((prediction-Y_train)))
+
+        validation_error = J(X_valid_complex,Y_valid, theta_dg)
+        train_error = J(X_train_complex,Y_train, theta_dg)
+        
+        v.append(validation_error)
+        t.append(train_error)
+
+        valid_error_list.append(validation_error)
+        # print(mean_valid_error, sd_valid_error)
+
+        # Ref Engelbretch (IC), Eq 7.7, pag 96
+        #if (validation_error > (mean_valid_error + sd_valid_error) and num_it > 1) or num_it >= max_interations_allowed:
+        if validation_error > validation_error_prev or num_it >= max_interations_allowed:
+        # if num_it >= max_interations_allowed:
+            break
+
+        mean_valid_error = np.mean(np.asarray(valid_error_list))
+        sd_valid_error = np.std(np.asarray(valid_error_list))
+        validation_error_prev = validation_error
+
+        num_it += 1
+    
+    for i in range(len(t)):
+      plt.plot(i, t[i], marker='o', color='black', linestyle='None')
+      plt.plot(i, v[i], marker='x', color='red', linestyle='None')
+        
+    plt.show()
+
+    return theta_dg, num_it
+
+
 ########################### 
 
 if __name__ == "__main__":
