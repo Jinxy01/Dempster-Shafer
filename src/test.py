b1b4872dac2d7ea9135aff23ca74c93dc6b92528
Tiago Roxo <tfroxo@hotmail.com>
2020-12-22 14:07:36 +0000

    Multiple rules but no MAF yet

--- src/test.py
+++ src/test.py
@@ -62,9 +62,9 @@ def gradient_descent(theta_dg, learning_rate, X_train_complex, X_valid_complex):
 # --------------------------------------
 
 
-def mse(Y, Y_hat):
+def mse(y, y_hat):
     # Y_hat is the predicted one
-    return np.square(np.subtract(Y,Y_hat)).mean() 
+    return (y - y_hat).pow(2).mean()
 
 def is_converged(loss_current, loss_previous, tot_elements):
     epsilon=0.0001
@@ -72,11 +72,6 @@ def is_converged(loss_current, loss_previous, tot_elements):
     #print(np.size(convergence) - np.count_nonzero(convergence))
     # All rules have converged to minimal loss
     return np.count_nonzero(convergence) == tot_elements
-    
-
-def predict_y(m):
-    y_hat = 1*m[frozenset('R')] + 0*m[frozenset('B')] + 0.5*m[frozenset({'R','B'})]
-    return y_hat, [m[frozenset('R')], m[frozenset('B')], m[frozenset({'R','B'})]]
 
 def update_rule(R, B, R_B):
     m = {}
@@ -95,6 +90,15 @@ def get_rule(rules, y):
     else: # y > 0.42
         return 4
 
+def get_rule_temp(rules, y):
+    rule_id = -1
+    if y <= 0:
+        rule_id = 1
+    else: # y > 0
+        rule_id = 2
+
+    return rules[rule_id][frozenset('B')], rules[rule_id][frozenset('R')], rules[rule_id][frozenset({'B','R'})]
+
 def project_masses(list_m):
     sum_m = 0
     for m in list_m:
@@ -104,105 +108,12 @@ def project_masses(list_m):
     if sum_m == 1.0:
         return list_m
 
-    list_m_nomr = []
+    list_m_norm = []
     for m in list_m:
-        list_m_nomr.append(m/sum_m)
+        list_m_norm.append(m/sum_m)
     
-    return list_m_nomr
+    return list_m_norm
 
-
-# Not working... need gradient of loss function...
-def train_prev(X_train, Y_train, rules):
-    converged = False
-    adam = adam_optimizer(alpha=0.002)
-    tot_elements = len(X_train)
-    previous_loss = np.zeros(tot_elements)
-    dict_it_rule = {1: 0, 2: 0, 3: 0, 4: 0} 
-    it = 0
-    while not converged:
-        current_loss = []
-        for i in range(tot_elements):
-            [x, y] = X_train[i]
-            #print(x, y)
-            id_rule = get_rule(rules, y)
-            dict_it_rule[id_rule] += 1
-            # Not using MAF, yet
-            m = rules[id_rule]
-            y_hat, theta = predict_y(m)
-            #print(Y_train[i])
-            loss = mse(y_hat, Y_train[i])
-            print(x, y, Y_train[i], loss)
-            current_loss.append(loss)
-            #print(theta)
-            theta = adam.update(theta, loss, dict_it_rule[id_rule]) # Adam update basen on number of changes of the used rule
-            #print(theta)
-            #print(rules[id_rule])
-            theta_projected = project_masses(theta)
-            rules[id_rule] = update_rule(theta[0], theta[1], theta[2])
-
-        current_loss_array = np.array(current_loss)
-        converged = is_converged(current_loss, previous_loss, tot_elements)
-        previous_loss = np.copy(current_loss_array)
-        it += 1
-        if it > 10:
-            break
-    return rules, current_loss_array, dict_it_rule
-
-def train(X_train, Y_train, rules):
-    converged = False
-    adam = adam_optimizer(alpha=0.002)
-    tot_elements = len(X_train)
-    previous_loss = np.zeros(tot_elements)
-    dict_it_rule = {1: 0, 2: 0, 3: 0, 4: 0} 
-    it = 0
-
-    dtype = torch.float
-    device = torch.device("cpu")
-    a = torch.randn((), device=device, dtype=dtype, requires_grad=True)
-    b = torch.randn((), device=device, dtype=dtype, requires_grad=True)
-    c = torch.randn((), device=device, dtype=dtype, requires_grad=True)
-    d = torch.randn((), device=device, dtype=dtype, requires_grad=True)
-
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-
-    print(optimizer)
-    while e in range(10):
-        current_loss = []
-        for i in range(tot_elements):
-            [x, y] = X_train[i]
-            #print(x, y)
-            id_rule = get_rule(rules, y)
-            dict_it_rule[id_rule] += 1
-            # Not using MAF, yet
-            m = rules[id_rule]
-            y_hat, theta = predict_y(m)
-            #print(Y_train[i])
-            loss = mse(y_hat, Y_train[i])
-            print(x, y, Y_train[i], loss)
-            current_loss.append(loss)
-            #print(theta)
-            theta = adam.update(theta, loss, dict_it_rule[id_rule]) # Adam update basen on number of changes of the used rule
-            #print(theta)
-            #print(rules[id_rule])
-            theta_projected = project_masses(theta)
-            rules[id_rule] = update_rule(theta[0], theta[1], theta[2])
-
-        current_loss_array = np.array(current_loss)
-        converged = is_converged(current_loss, previous_loss, tot_elements)
-        previous_loss = np.copy(current_loss_array)
-        it += 1
-        if it > 10:
-            break
-    return rules, current_loss_array, dict_it_rule
-
-def get_belief_t(r, b, r_b):
-    dtype = torch.float
-    device = torch.device("cpu")
-    belief = max(r, b, r_b)
-    #if r > b:
-    #    return torch.tensor(1.0, device=device, dtype=dtype, requires_grad=True)
-    #return torch.tensor(0.0, device=device, dtype=dtype, requires_grad=True)
-    return belief
 
 def get_belief_confidence(r, b, r_b):
     dtype = torch.float
@@ -214,9 +125,83 @@ def get_belief_confidence(r, b, r_b):
     return -b/(b+r_b)
 
 
+def testing_stuff(X_train, Y_train, rules):
 
+    dtype = torch.float
+    device = torch.device("cpu")
+    # device = torch.device("cuda:0")  # Uncomment this to run on GPU
 
-def testing_stuff(X_train, Y_train):
+    # Create Tensors to hold input and outputs.
+    # By default, requires_grad=False, which indicates that we do not need to
+    # compute gradients with respect to these Tensors during the backward pass.
+    #x = torch.linspace(-math.pi, math.pi, 2000, device=device, dtype=dtype)
+    #y = torch.sin(x)
+    tot = len(X_train)
+
+    # Create random Tensors for weights. For a third order polynomial, we need
+    # 4 weights: y = a + b x + c x^2 + d x^3
+    # Setting requires_grad=True indicates that we want to compute gradients with
+    # respect to these Tensors during the backward pass.
+
+    learning_rate = 1e-3
+    previous_loss = sys.maxsize
+    for t in range(2000):
+        for i in range(tot):
+            x, y = X_train[i]
+
+            r, b, r_b = get_rule_temp(rules, y)
+            #print(r, b, r_b)
+            # Forward pass: compute predicted y using operations on Tensors.
+            #y_pred = a + b * x + c * x ** 2 + d * x ** 3
+            y_hat = get_belief_confidence(r, b, r_b)
+            #y_hat = (r + 0.5*r_b)/(r+b+r_b)
+            #print(y_hat)
+            
+            # Compute and print loss using operations on Tensors.
+            # Now loss is a Tensor of shape (1,)
+            # loss.item() gets the scalar value held in the loss.
+            # loss = (y_pred - y).pow(2).sum()
+            y = Y_train[i] # Predicted class
+
+            loss = mse(y, y_hat)
+            
+            if t % 100 == 99:
+                print(t, loss.item())
+
+            if is_converged(loss, previous_loss, 1):
+                print("Breaking at {} iteration".format(t))
+                break
+
+            previous_loss = loss 
+            # Use autograd to compute the backward pass. This call will compute the
+            # gradient of loss with respect to all Tensors with requires_grad=True.
+            # After this call a.grad, b.grad. c.grad and d.grad will be Tensors holding
+            # the gradient of the loss with respect to a, b, c, d respectively.
+            loss.backward()
+
+            # Manually update weights using gradient descent. Wrap in torch.no_grad()
+            # because weights have requires_grad=True, but we don't need to track this
+            # in autograd.
+            with torch.no_grad():
+                if r.grad is not None:
+                    r -= learning_rate * r.grad
+                if b.grad is not None:
+                    b -= learning_rate * b.grad
+                if r_b.grad is not None:
+                    r_b -= learning_rate * r_b.grad
+
+                # Manually zero the gradients after updating weights
+                r.grad = None
+                b.grad = None
+                r_b.grad = None
+
+    for _, weights in rules.items():
+        r, b, r_b = weights[frozenset('B')], weights[frozenset('R')], weights[frozenset({'B','R'})]
+        print(f'Result: r = {r.item()}, b = {b.item()}, r_b = {r_b.item()}')
+        l_proj = project_masses([r.item(), b.item(), r_b.item()])
+        print(l_proj)
+
+def working_stuff(X_train, Y_train):
 
     dtype = torch.float
     device = torch.device("cpu")
@@ -253,7 +238,8 @@ def testing_stuff(X_train, Y_train):
         # loss = (y_pred - y).pow(2).sum()
         y = Y_train[0] # Predicted class
 
-        loss = (y_hat - y).pow(2).sum()
+        loss = mse(y, y_hat)
+        
         if t % 100 == 99:
             print(t, loss.item())
 
@@ -291,28 +277,30 @@ def testing_stuff(X_train, Y_train):
 #--------------------------
 
 def get_initial_masses():
+    dtype = torch.float
+    device = torch.device("cpu")
     m = {}
-    m[frozenset('B')] = 0.04 # Blue
-    m[frozenset('R')] = 0.06 # Red
-    m[frozenset({'B','R'})] = 0.9 # Uncertainty
+    m[frozenset('B')] = torch.tensor(0.04, device=device, dtype=dtype, requires_grad=True)
+    m[frozenset('R')] = torch.tensor(0.06, device=device, dtype=dtype, requires_grad=True)
+    m[frozenset({'B','R'})] = torch.tensor(0.9, device=device, dtype=dtype, requires_grad=True) # Uncertainty
     return m
 
 def start_rules():
     rules = {}
     rules[1] = get_initial_masses()
     rules[2] = get_initial_masses()
-    rules[3] = get_initial_masses()
-    rules[4] = get_initial_masses()
+    #rules[3] = get_initial_masses()
+    #rules[4] = get_initial_masses()
     return rules
 
 if __name__ == "__main__":
     #X_train, Y_train, X_test, Y_test = aid_test()
     # Testing purposes
-    X_train = [np.array([-0.2, -0.3]), np.array([-0.3, -0.4])]
-    Y_train = [0, 1] 
-    testing_stuff(X_train, Y_train)
-    exit(0)
     rules = start_rules()
+    X_train = [np.array([-0.2, -0.3]), np.array([-0.3, 0.4])]
+    Y_train = [-1, 1] 
+    testing_stuff(X_train, Y_train, rules)
+    exit(0)
     #print(rules)
     rules, current_loss_array, dict_it_rule = train(X_train, Y_train, rules)
     exit(0)
