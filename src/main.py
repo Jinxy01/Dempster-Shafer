c9f1a37e06ddf4f7f9a521220b5fcb8591ca3b55
Tiago Roxo <tfroxo@hotmail.com>
2021-01-01 22:51:46 +0000

    HD done

--- src/main.py
+++ src/main.py
@@ -190,6 +190,6 @@ def evaluate_wine_dataset(dataset_name):
 if __name__ == "__main__":
     #evaluate_A1_dataset("A1_Dataset")
     #evaluate_breast_cancer_dataset("BC_Dataset")
-    #evaluate_iris_dataset("IRIS_Dataset")
-    evaluate_heart_disease_dataset("HD_Dataset")
+    evaluate_iris_dataset("IRIS_Dataset")
+    #evaluate_heart_disease_dataset("HD_Dataset")
     #evaluate_wine_dataset("WINE_Dataset")
